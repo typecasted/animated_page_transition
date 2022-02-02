8d12456ce81c2cb72fa35a80b46266c76d8dc3cf
Kunal Pithadiya <typecasted1@gmail.com>
2022-02-02 11:41:43 +0530

    changes in documentation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 0.0.1+2
+changes in documentation.
+
 ## 0.0.1+1
 changes in README.md
 
--- lib/src/page_transition_src.dart
+++ lib/src/page_transition_src.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/animation.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 
